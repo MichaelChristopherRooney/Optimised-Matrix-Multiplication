afb1cf5347e0190dcc1089195a4432002152d357
Michael Rooney <m.rooney93@gmail.com>
2015-05-10 15:18:35 -0700

    Significant rework to make code easier to read, readme updated

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-DavidGreggLabOne
-================
+#About
+
+
+A program to speed up matrix multiplication.
+
+
+Uses OpenMP to compute results in parallel and transposing to optimise cache usage (see http://www.akkadia.org/drepper/cpumemory.pdf Section 6.2.1)
+
+
+The dimensions of the matrices are passed as arguments to the program, in the form: "./matmul <A rows> <A cols> <B rows> <B cols>"
+
+
+#Timings
+
+Timed using an Intel i5 2500k CPU.
+
+
+Each category was run 10 times and the given result is an average of the timings.
+
+
+In all categories two 1000x1000 matrices were multiplied.
+
+
+Compiled with "gcc -fopenmp matmul.c"
+
+
+	- Normal matrix multiplication: 10.31 seconds
+	- Matrix multiplication using a transposed matrix: 3.53 seconds
+	- Matrix multiplication with OpenMP: 2.91 seconds
+	- Matrix multiplication using OpenMP and a transposed Matrix: 1.13 seconds
+
+
+These numbers show that using OpenMP and transposing results a program that on average runs 9.1x faster
+
+#Issues
+
+Running this program with very small matrices will result in the "optimised" version being slower.
+
+
+This is due to the overhead of creating threads and transposing the matrix being larger than the time saved.
+
+
+
---
+++ matmul.c
@@ -0,0 +1,209 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+/* create new empty matrix */
+double ** createEmptyMatrix(int dimOne, int dimTwo){
+
+	double ** result = malloc(sizeof(double*) * dimOne);
+	double * new_matrix = malloc(sizeof(double) * dimOne * dimTwo);
+	
+	int i;
+	for ( i = 0; i < dimOne; i++ ) {
+		result[i] = &(new_matrix[i*dimTwo]);
+	}
+
+	return result;
+
+}
+
+double ** generateRandomMatrix(int dimOne, int dimTwo){
+
+	double ** temp = createEmptyMatrix(dimOne, dimTwo);
+
+	srandom(time(NULL));
+
+	int i, n;
+	for(i = 0; i < dimOne; i++){
+		for(n = 0; n < dimTwo; n++){
+			temp[i][n] = random();
+			//printf("%f\n", temp[i][n]);
+		}
+	}
+
+	return temp;
+
+}
+
+void multiplyMatices(double ** A, double ** B, double ** result, int aDim, int sharedDim, int bDim){
+
+	int i, j, k;
+	for(i = 0; i < aDim; i++){
+		for(j = 0; j < bDim; j++){
+
+			double sum = 0.0;
+			for(k = 0; k < sharedDim; k++){
+				sum += A[i][k] * B[k][j];
+			}
+
+			result[i][j] = sum;
+
+		}
+	}
+
+}
+
+double ** transpose(double ** p, int dimOne, int dimTwo){
+
+	double ** t = createEmptyMatrix(dimTwo, dimOne);
+
+	int i, j;
+	for(i = 0; i < dimOne; i++){
+		for(j = 0; j < dimTwo; j++){
+			t[j][i] = p[i][j];
+		}
+	}
+
+	return t;
+
+}
+
+void multiplyMatricesTransposeOnly(double ** A, double ** pB, double ** result, int aDim, int sharedDim, int bDim){
+
+	// new matrix is created here to avoid changing B in the main function
+	double ** B = transpose(pB, sharedDim, bDim);
+
+	int i;
+	for(i = 0; i < aDim; i++){
+
+		int j, k;
+		for(j = 0; j < bDim; j++){
+
+			double sum = 0.0;
+			for(k = 0; k < sharedDim; k++){
+				sum += A[i][k] * B[j][k];
+			}
+
+			result[i][j] = sum;
+
+		}
+
+	}
+
+}
+
+void multiplyMatricesThreadedOnly(double ** A, double ** B, double ** result, int aDim, int sharedDim, int bDim){
+
+	int i;
+	#pragma omp parallel for
+	for(i = 0; i < aDim; i++){
+
+		int j, k;
+		for(j = 0; j < bDim; j++){
+
+			double sum = 0.0;
+			for(k = 0; k < sharedDim; k++){
+				sum += A[i][k] * B[k][j];
+			}
+
+			result[i][j] = sum;
+
+		}
+
+	}
+
+}
+
+void multiplyMatricesOptimised(double ** A, double ** B, double ** result, int aDim, int sharedDim, int bDim){
+
+	B = transpose(B, sharedDim, bDim);
+
+	int i;
+	#pragma omp parallel for
+	for(i = 0; i < aDim; i++){
+
+		int j, k;
+		for(j = 0; j < bDim; j++){
+
+			double sum = 0.0;
+			for(k = 0; k < sharedDim; k++){
+				sum += A[i][k] * B[j][k];
+			}
+
+			result[i][j] = sum;
+
+		}
+
+	}
+
+}	
+
+void checkResults(double ** rOne, double ** rTwo, int dimOne, int dimTwo){
+
+	int i, j;
+	int error = 0;
+
+	for(i = 0; i < dimOne; i++){
+		for(j = 0; j < dimTwo; j++){
+
+			if(rOne[i][j] != rTwo[i][j]){
+				error = 1;
+			}
+
+		}
+	}
+
+	if(error){
+		printf("ERROR: Matrices do not match\n");
+	}else{
+		printf("OK: Matrices match\n");
+	}
+
+}
+
+int main(int argc, char** argv){
+
+	int aDimOne, aDimTwo, bDimOne, bDimTwo;
+	struct timeval startTime; struct timeval stopTime;
+	long long originalTime = 0L; long long optimisedTime = 0L;
+
+	if(argc != 5){
+		printf("Error: parameters not provided.\n");
+		return -1;
+	}
+
+	aDimOne = atoi(argv[1]);
+	aDimTwo = atoi(argv[2]);
+	bDimOne = atoi(argv[3]);
+	bDimTwo = atoi(argv[4]);
+
+	if(aDimTwo != bDimOne){
+		printf("Number of columns in A does not match number of rows in B\n");
+		return -1;
+	}
+
+	double ** A = generateRandomMatrix(aDimOne, aDimTwo);
+	double ** B = generateRandomMatrix(bDimOne, bDimTwo);
+	double ** originalResult = createEmptyMatrix(aDimOne, bDimTwo);
+	double ** optimisedResult = createEmptyMatrix(aDimOne, bDimTwo);
+
+	// time unoptimised multiplication
+	gettimeofday(&startTime, NULL);
+	multiplyMatices(A, B, originalResult, aDimOne, aDimTwo, bDimTwo);
+	gettimeofday(&stopTime, NULL);
+	originalTime += (stopTime.tv_sec - startTime.tv_sec) * 1000000L + (stopTime.tv_usec - startTime.tv_usec);
+
+
+	// time optimised multiplication
+	gettimeofday(&startTime, NULL);
+	multiplyMatricesOptimised(A, B, optimisedResult, aDimOne, aDimTwo, bDimTwo);
+	gettimeofday(&stopTime, NULL);
+	optimisedTime = (stopTime.tv_sec - startTime.tv_sec) * 1000000L + (stopTime.tv_usec - startTime.tv_usec);
+
+	checkResults(originalResult, optimisedResult, aDimOne, bDimTwo);
+
+	printf("Unoptimised multiplication took: %lld microseconds\n", originalTime / 10);
+	printf("Unoptimised multiplication took: %lld microseconds\n", optimisedTime / 10);
+
+}
